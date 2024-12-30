9a204354559de3ece56a6174d3f429d0784ee863
andiasrafil <41411347+andiasrafil@users.noreply.github.com>
2024-12-31 01:10:27 +0700

    feat(teka_teki_tiku): Melos mantap

--- apps/teka_teki_tiku/lib/counter/view/counter_page.dart
+++ apps/teka_teki_tiku/lib/counter/view/counter_page.dart
@@ -21,7 +21,7 @@ class CounterView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('Toko Toko Toko')),
+      appBar: AppBar(title: const Text('Melos Mantap')),
       body: CrosswordWidget(
         crossWordModel: dummyCrossWordModel,
       ),
