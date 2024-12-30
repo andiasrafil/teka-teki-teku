1b06764c492fdae6c506357e68744d5dbde03bef
andiasrafil <41411347+andiasrafil@users.noreply.github.com>
2024-12-31 01:04:14 +0700

    feat(teka_teki_tiku): toko toko toki

--- apps/teka_teki_tiku/lib/counter/view/counter_page.dart
+++ apps/teka_teki_tiku/lib/counter/view/counter_page.dart
@@ -21,7 +21,7 @@ class CounterView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('Toko Toko')),
+      appBar: AppBar(title: const Text('Toko Toko Toki')),
       body: CrosswordWidget(
         crossWordModel: dummyCrossWordModel,
       ),
