1e623cc1963c77947b2c9e800b99d576f495f259
andiasrafil <41411347+andiasrafil@users.noreply.github.com>
2024-12-30 16:37:03 +0700

    feat(apps/teka_teki_tiku): update appbar title

--- apps/teka_teki_tiku/lib/counter/view/counter_page.dart
+++ apps/teka_teki_tiku/lib/counter/view/counter_page.dart
@@ -21,7 +21,7 @@ class CounterView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('Teka Teki Tiku')),
+      appBar: AppBar(title: const Text('Teka Teki Tiku Tuku')),
       body: CrosswordWidget(
         crossWordModel: dummyCrossWordModel,
       ),
