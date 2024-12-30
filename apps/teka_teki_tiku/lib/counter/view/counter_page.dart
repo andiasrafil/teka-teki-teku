e3ae2218bc1231a0fbc49ba4ecd4ad8cc48a5a89
andiasrafil <41411347+andiasrafil@users.noreply.github.com>
2024-12-30 20:39:41 +0700

    feat(teka_teki_tiku): update apa tau

--- apps/teka_teki_tiku/lib/counter/view/counter_page.dart
+++ apps/teka_teki_tiku/lib/counter/view/counter_page.dart
@@ -21,7 +21,7 @@ class CounterView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('Teka Teki')),
+      appBar: AppBar(title: const Text('Teka Teki Tiki')),
       body: CrosswordWidget(
         crossWordModel: dummyCrossWordModel,
       ),
