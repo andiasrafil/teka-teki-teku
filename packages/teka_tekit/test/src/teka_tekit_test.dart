c4c02396328160c4cd1d5f1a7af89cf14bafdddf
andiasrafil <41411347+andiasrafil@users.noreply.github.com>
2024-12-30 14:55:19 +0700

    chore: update unit test

--- packages/teka_tekit/test/src/teka_tekit_test.dart
+++ packages/teka_tekit/test/src/teka_tekit_test.dart
@@ -1,11 +1,38 @@
 // ignore_for_file: prefer_const_constructors
 
+import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:patrol/patrol.dart';
+import 'package:teka_tekit/teka_tekit.dart';
 
 void main() {
   group('TekaTekit', () {
-    // test('can be instantiated', () {
-    //   expect(TekaTekit(), isNotNull);
-    // });
+    patrolWidgetTest('Test widget', (tester) async {
+      await tester.pumpWidget(
+        MaterialApp(
+          home: Scaffold(
+            body: DisabledCell(),
+          ),
+        ),
+      );
+      expect(tester(DisabledCell), findsOneWidget);
+    });
+
+    patrolWidgetTest('Enabled Cell', (tester) async {
+      await tester.pumpWidget(
+        MaterialApp(
+          home: Scaffold(
+            body: EnabledCell(
+              activeModel:
+                  ActiveCellModel(0, 0, TextEditingController(), FocusNode()),
+              isSelected: false,
+              isFocused: false,
+              text: 'A',
+            ),
+          ),
+        ),
+      );
+      expect(tester(EnabledCell), findsOneWidget);
+    });
   });
 }
